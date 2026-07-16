4f04b49ee48e06ce514c9102b1b09793cbd66ed9
RqtHqck <rathack13@gmail.com>
2026-07-16 23:20:01 +0300

    patch: - auth proto - proto reusable config

---
+++ src/constants/common.ts
@@ -0,0 +1,13 @@
+import { join } from 'path';
+
+export const PROTO_PATH = 'node_modules/@kreqel/common/constants';
+
+interface Package {
+    package: string;
+    protoPath: string;
+}
+
+export const AUTH_PACKAGE: Package = {
+    package: 'auth.v1',
+    protoPath: join(PROTO_PATH, 'auth.proto'),
+};
