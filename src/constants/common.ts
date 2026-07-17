700a3aa7eff023877df5296728e44d92edab61c7
RqtHqck <rathack13@gmail.com>
2026-07-17 11:37:55 +0300

    patch: - add token

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -5,9 +5,11 @@ export const PROTO_PATH = 'node_modules/@kreqel/common/dist/proto';
 interface Package {
     package: string;
     protoPath: string;
+    token: string;
 }
 
 export const AUTH_PACKAGE: Package = {
     package: 'auth.v1',
+    token: 'AUTH_PACKAGE',
     protoPath: join(PROTO_PATH, 'auth.proto'),
 };
